b237d7a563299d7db354294d5acc88950099a587
mark aerol tomarse <aerolpogi233@gmail.com>
2023-03-09 21:49:06 +0800

    fix api routes

--- api/authen/index.js
+++ api/authen/index.js
@@ -1,114 +1,109 @@
-const router = require('express').Router()
-const mongoose = require('mongoose')
-const Models = require('../../models/index')
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const Models = require("../../models/index");
 
-const {Officers, Owners} = Models
+const { Officers, Owners } = Models;
 
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+  try {
+    let officers = await Officers.find(req.body);
+    let owners = await Owners.find(req.body);
 
-
-
-router.post('/login',  async (req, res) => {
-    console.log(req.body)
-    try{
-        let officers = await Officers.find(req.body)
-        let owners = await Owners.find(req.body)
-
-        const merge = [...officers, ...owners]
-        if(merge.length == 0) {
-            return res.status(200).json({
-                msg:'Invalid account. Please register or try again!',
-                result:false, 
-                type:'warning'
-            })
-        }
-        
-        console.log("MERON")
-        console.log(merge)
-        res.status(200).json({
-            msg:"You logged in!",
-            result:true,
-            type:'success',
-            user:merge[0],
-        })
-
-        
-    }catch(err){
-        console.log(err)
-        res.json({
-            msg:err,
-            result:false,
-            type:'error'
-        })
+    const merge = [...officers, ...owners];
+    if (merge.length == 0) {
+      return res.status(200).json({
+        msg: "Invalid account. Please register or try again!",
+        result: false,
+        type: "warning"
+      });
     }
-})
 
-router.get('/logout',  async (req, res) => {
-    try{
-        res.json({
-            msg:"Logout success!"
-        })
-    }catch(err){
-        console.log(err)
-        res.json({
-            err
-        })
+    console.log("MERON");
+    console.log(merge);
+    res.status(200).json({
+      msg: "You logged in!",
+      result: true,
+      type: "success",
+      user: merge[0]
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      msg: err,
+      result: false,
+      type: "error"
+    });
+  }
+});
+
+router.get("/logout", async (req, res) => {
+  try {
+    res.json({
+      msg: "Logout success!"
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      err
+    });
+  }
+});
+
+router.get("/user", (req, res) => {
+  res.json({ msg: "" });
+});
+
+router.post("/signup/sendcode", async (req, res) => {
+  try {
+    const { code, email } = req.body;
+    console.log(req.body);
+    if (isEmailExist(email) == true) {
+      return res.json({ msg: "Your email is already exist.", result: false });
     }
-})
+    console.log(req.body);
+    const message = {
+      from: "marktomarse@gmail.com", // Sender address
+      to: email, // List of recipients
+      subject: "Account Verificationn", // Subject line
+      text:
+        "hello, this is your code account verification from SM CINEMA RESERVATION. \n CODE: " +
+        code // Plain text body
+    };
+    transport.sendMail(message, function(err, info) {
+      if (err) {
+        console.log(err);
+        res.json({ msg: err, result: false });
+      } else {
+        console.log(info);
+        res.json({ msg: "Your code successfuly send." });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({ msg: err, result: false });
+  }
+});
 
-router.get('/user', (req, res) => {
-    res.json({msg:''})
-})
+router.post("/signup/register", async (req, res) => {
+  try {
+    let newcustomer = new Customers(req.body);
+    console.log(newcustomer);
+    newcustomer.save();
 
-router.post('/signup/sendcode', async (req, res) => {
-    try{
-        
-        const {code, email} = req.body
-        console.log(req.body)
-        if(isEmailExist(email) == true) {
-            return res.json({msg:"Your email is already exist.", result:false})
-        }
-        console.log(req.body)
-        const message = {
-            from: 'marktomarse@gmail.com', // Sender address
-            to: email,         // List of recipients
-            subject: 'Account Verificationn', // Subject line
-            text: 'hello, this is your code account verification from SM CINEMA RESERVATION. \n CODE: '+code // Plain text body
-        };
-        transport.sendMail(message, function(err, info) {
-            if (err) {
-                console.log(err)
-                res.json({msg:err, result:false})
-            } else {
-                console.log(info)
-                res.json({msg:'Your code successfuly send.'})
-            }
-        });
-    }catch(err){
-        console.log(err)
-        res.json({msg:err, result:false})
-    }
-})
+    res.json({ msg: "You now registered!", user: newcustomer });
+  } catch (err) {
+    res.json({ msg: err, result: false });
+  }
+});
 
-router.post('/signup/register', async (req, res) => {
-    try{
-        let newcustomer = new Customers(req.body)
-        console.log(newcustomer)
-        newcustomer.save()
+async function isEmailExist(email) {
+  let customer = await Customers.find({ email: email.toLowerCase() });
 
-        res.json({msg:'You now registered!', user:newcustomer})
-    }catch(err){
-        res.json({msg:err, result:false})
-    }
-})
-
-async function isEmailExist(email){
-    
-    let customer = await Customers.find({email:email.toLowerCase()})
-    
-    if(customer.length > 0) {
-        return true
-    }
-    return false
+  if (customer.length > 0) {
+    return true;
+  }
+  return false;
 }
 
-module.exports = router
+module.exports = router;
--- api/owner/index.js
+++ api/owner/index.js
@@ -1,90 +1,84 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
+const multer = require("multer");
+const uuid = require("uuid");
+const moment = require("moment");
+const { Officers, Owners, Violations } = require("../../models/index");
+const path = require("path");
 
-const multer = require('multer')
-const uuid = require('uuid')
-const moment = require('moment')
-const { Officers, Owners, Violations } = require('../../models')
-const path = require('path')
+function upload(filename) {
+  let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, `./assets/uploads/`); //
+    },
+    filename: function(req, file, cb) {
+      console.log(file);
+      cb(null, filename + path.extname(file.originalname)); //Appending extension
+    }
+  });
 
-
-
-function upload(filename){
-    
-    let storage = multer.diskStorage({
-        destination: function (req, file, cb) {           
-          cb(null, `./assets/uploads/`) // 
-        },
-        filename: function (req, file, cb) {
-            console.log(file)
-            cb(null, filename + path.extname(file.originalname)) //Appending extension
-        }
-    })
-    
-    //Multer middleware config
-    return multer({ storage: storage})
+  //Multer middleware config
+  return multer({ storage: storage });
 }
 
+router.post("/new", async (req, res) => {
+  try {
+    console.log(req.body);
+    let invalids = [];
+    if (
+      (await Owners.find({ email: req.body.email })).length > 0 ||
+      (await Officers.find({ email: req.body.email })).length > 0
+    )
+      invalids.push("Your email is not available.");
+    if (
+      (await Owners.find({ serial_number: req.body.serial_number })).length > 0
+    )
+      invalids.push("Your serial number is not available. you may report it");
 
+    if (invalids.length > 0) return res.json({ result: false, invalids });
+    const filename = uuid.v4();
+    req.body.driverlicenseimg = filename + req.body.driverlicenseimg;
+    let newowner = new Owners(req.body);
+    newowner.save();
 
-router.post('/new', async (req, res) => {
-    try{
-       
-        console.log(req.body)
-        let invalids = []
-        if((await Owners.find({email:req.body.email})).length > 0 || (await Officers.find({email:req.body.email})).length > 0) invalids.push('Your email is not available.')
-        if((await Owners.find({serial_number:req.body.serial_number})).length > 0) invalids.push('Your serial number is not available. you may report it')
+    //upload().single('licenseimg')
+    res.json({ newowner, result: true, filename });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ result: false, msg: err });
+  }
+});
 
-        if(invalids.length > 0) return res.json({result:false, invalids})
-        const filename = uuid.v4()
-        req.body.driverlicenseimg = filename + req.body.driverlicenseimg
-        let newowner = new Owners(req.body)
-        newowner.save()
-
-        //upload().single('licenseimg')
-        res.json({newowner, result:true, filename})
-    }catch(err){
-        console.log(err)
-        res.status(400).json({result:false, msg:err})
-    }
-})
-
-router.post('/violations', async (req, res) => {
-    console.log(req.body)
-    let violations = await Violations.find({ownerID:req.body.ownerID})
-    console.log(violations)
-    res.json({violations, result:true})
-})
-
-
-
+router.post("/violations", async (req, res) => {
+  console.log(req.body);
+  let violations = await Violations.find({ ownerID: req.body.ownerID });
+  console.log(violations);
+  res.json({ violations, result: true });
+});
 
 //SINGE FILE UPLOAD ROUTES
-router.post('/singleupload',  async (req, res) => {
-    let invalids = []
-    const filename = req.query.filename
-    
-    try{
-        upload(filename).single('licenseimg')(req, res, (error) => {
-            if (error) {
-                console.log(error)
-                invalids.push(`File upload error: ${error}`)
-                return res.status(500).json({invalids, result:false});
-            }
-            
-            res.status(200).json({msg:'File uploaded', filename, result:true})
-        // code
-        })
-    
-        return
-    }catch(error){
-        console.log(error)
-        invalids.push(`File upload error: ${error}`)
-        return res.status(500).json({invalids, result:false});
-    }
-    
-})
+router.post("/singleupload", async (req, res) => {
+  let invalids = [];
+  const filename = req.query.filename;
 
+  try {
+    upload(filename).single("licenseimg")(req, res, error => {
+      if (error) {
+        console.log(error);
+        invalids.push(`File upload error: ${error}`);
+        return res.status(500).json({ invalids, result: false });
+      }
 
+      res.status(200).json({ msg: "File uploaded", filename, result: true });
+      // code
+    });
 
-module.exports = router
+    return;
+  } catch (error) {
+    console.log(error);
+    invalids.push(`File upload error: ${error}`);
+    return res.status(500).json({ invalids, result: false });
+  }
+});
+
+module.exports = router;
